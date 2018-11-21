d67b5ba6dc62280962d6391ecf3194ef18060d7d
KendalHolcombe <kendal.chilcott2@yahoo.com>
2018-11-21 11:08:55 -0600

    morning changes

--- Query_Script.py
+++ Query_Script.py
@@ -4,7 +4,14 @@ import json
 
 
 def Query_to_DF(query):
-    
+    ''' Pass string query in and return a dataframe of query results
+
+    INPUT: string
+    OUTPUT: dataframe
+
+    '''
+
+    # Pull postgres details from config file to make connection
     with open('config.json') as f:
         conf = json.load(f)
         host = conf['host']
@@ -12,9 +19,11 @@ def Query_to_DF(query):
         user = conf['user']
         passw = conf['passw']
 
+    # Create postgres connection
     conn_str = "host={} dbname={} user={} password={}".format(host, database, user, passw)
     conn = psycopg2.connect(conn_str)
 
+    # Run query and return dataframe
     df = pd.read_sql(query, con=conn)
     return df
 
