1dc03014266061c5699cc3b9387be429f61e3a04
KendalHolcombe <kendal.chilcott2@yahoo.com>
2018-11-27 16:34:50 -0600

    intelligent model scoped out

--- Features_Script.py
+++ Features_Script.py
@@ -14,14 +14,8 @@ def feature_engineering(train_df, test_df):
     # Drop NA in victim age column (<2%)
     train_df.dropna(axis=0, subset=['age_num'], inplace=True)
 
-    # Get crime count by county
-    crime_df = train_df.groupby(['county']).incident_id.agg('count')
-    crime_df = crime_df.to_frame().reset_index()
-    crime_df = crime_df.rename({'incident_id': 'crime_cnt'}, axis='columns')
-    train_df.join(crime_df.set_index('county'), on='county')
-
-    # Clean up count columns
-    train_df['beds'] = train_df['beds'].clip_lower(0)
+    # Drop NA in hospital_cnt column (<2%)
+    train_df.dropna(axis=0, subset=['hosp_cnt'], inplace=True)
 
     # Create Ratio Columns
     train_df['crime_pop_ratio'] = train_df['crime_cnt'] / train_df['population']
@@ -31,26 +25,20 @@ def feature_engineering(train_df, test_df):
     train_df['fire_crime_ratio'] = train_df['fire_cnt'] / train_df['crime_cnt']
 
     # Reduce df to only desired features to train/test model
-    train_df = train_df[['age_num', 'victim_sex', 'offense_category', 'location_id', 'POPULATION_DESCRIPTION',
+    train_df = train_df[['age_num', 'victim_sex', 'offense_category', 'location_id', 'population_description',
                          'officers', 'civilians', 'crime_pop_ratio', 'beds_pop_ratio', 'beds_crime_ratio',
                          'fire_pop_ratio', 'fire_crime_ratio', 'county']]
 
     # Dummize features
-    train_df = pd.get_dummies(train_df, columns=['victim_sex', 'offense_category', 'location_id', 'POPULATION_DESCRIPTION'])
+    train_df = pd.get_dummies(train_df, columns=['victim_sex', 'offense_category', 'location_id', 'population_description'])
 
 
     ### TEST_DF ###
     # Drop NA in victim age column (<2%)
     test_df.dropna(axis=0, subset=['age_num'], inplace=True)
 
-    # Get crime count by county
-    crime_df = test_df.groupby(['county']).incident_id.agg('count')
-    crime_df = crime_df.to_frame().reset_index()
-    crime_df = crime_df.rename({'incident_id': 'crime_cnt'}, axis='columns')
-    test_df.join(crime_df.set_index('county'), on='county')
-
-    # Clean up count columns
-    test_df['beds'] = test_df['beds'].clip_lower(0)
+    # Drop NA in hospital_cnt column (<2%)
+    test_df.dropna(axis=0, subset=['hosp_cnt'], inplace=True)
 
     # Create Ratio Columns
     test_df['crime_pop_ratio'] = test_df['crime_cnt'] / test_df['population']
@@ -60,12 +48,12 @@ def feature_engineering(train_df, test_df):
     test_df['fire_crime_ratio'] = test_df['fire_cnt'] / test_df['crime_cnt']
 
     # Reduce df to only desired features to train/test model
-    test_df = test_df[['age_num', 'victim_sex', 'offense_category', 'location_id', 'POPULATION_DESCRIPTION',
+    test_df = test_df[['age_num', 'victim_sex', 'offense_category', 'location_id', 'population_description',
                        'officers', 'civilians', 'crime_pop_ratio', 'beds_pop_ratio', 'beds_crime_ratio',
                        'fire_pop_ratio', 'fire_crime_ratio', 'county']]
 
-    # Dummize features
-    test_df = pd.get_dummies(test_df, columns=['victim_sex', 'offense_category', 'location_id', 'POPULATION_DESCRIPTION'])
+    # Dummyize features
+    test_df = pd.get_dummies(test_df, columns=['victim_sex', 'offense_category', 'location_id', 'population_description'])
 
 
     # # Ensure features match across train and test dataframes
